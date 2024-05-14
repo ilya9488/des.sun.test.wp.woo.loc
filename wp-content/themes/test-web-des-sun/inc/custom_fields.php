24286f0e385abcf704bf71413b3cbccd3f9ca133
ILYA <62027029+ilya9488@users.noreply.github.com>
2024-05-14 14:51:57 +0300

    add: ACF to menu items

--- wp-content/themes/test-web-des-sun/inc/custom_fields.php
+++ wp-content/themes/test-web-des-sun/inc/custom_fields.php
@@ -151,3 +151,60 @@ function get_style_acf_gut_block($settings_sections, $block_class){
 	}
 	return $style;
 }
+
+// add ACF to menu items
+add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects', 10, 2);
+function my_wp_nav_menu_objects( $items, $args ) {
+  if( $args->theme_location == 'header_menu' ){
+    foreach( $items as &$item ) {
+      $to_the_right = get_field('to_the_right',$item);
+      $header_sub_menus = have_rows('header_sub_menus', $item);
+      $fields = get_fields($item);
+      $non_empty_fields = array_filter($fields, function($value) {
+        return !empty($value);
+      });
+      if($non_empty_fields && !empty($non_empty_fields)){
+        $item->classes[] = $header_sub_menus?'menu-item-has-children-acf':'';
+        $item->classes[] = $to_the_right?'to-the-right':'';
+        ob_start();
+        set_query_var( 'menu_item', $item );
+          get_template_part('template-parts/parts/header-sub-menu');
+        $sub_menu = ob_get_clean();
+        $item->acf_content = $sub_menu;
+      }
+    }
+  }
+  return $items;
+}
+
+// extract ACF content to <li>
+class My_ACF_Walker_Header_Menu extends Walker_Nav_Menu {
+  // Function to start the element
+  function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+    // Standard output of the menu element
+    $output .= '<li class="' . implode(' ', $item->classes) . '">';
+    // Add a link if it is
+    if ( $item->url && $item->url != '#' ) {
+      $output .= '<a href="' . $item->url . '">';
+      $output .= $item->title;
+      $output .= '</a>';
+    } else {
+      $output .= $item->title;
+    }
+
+    // We display an additional html if it is => ACF
+    if ( $item->acf_content ) {
+      $output .= $item->acf_content;
+    }
+  }
+  
+  // add wrapper to standart sub-menu wp
+  function start_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<div class='sub-menu__wrap'><ul class='sub-menu'>\n";
+  }
+  function end_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "$indent</ul></div>\n";
+  }
+}
