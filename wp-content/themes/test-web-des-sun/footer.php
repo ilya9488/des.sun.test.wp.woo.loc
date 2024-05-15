64c24973b6abc29023319515d220cbf55c8cbb17
ILYA <62027029+ilya9488@users.noreply.github.com>
2024-05-15 16:43:57 +0300

    create: footer structure - widgets

--- wp-content/themes/test-web-des-sun/footer.php
+++ wp-content/themes/test-web-des-sun/footer.php
@@ -1,13 +1,18 @@
   <footer class="footer">
     <div class="container">
       <div class="footer__wrap">
-        footer
-      </div>
-      <div class="footer__wrap">
-        <?php dynamic_sidebar( 'footer_col_1' ); ?>
-      </div>
-      <div class="footer__wrap">
-        <?php dynamic_sidebar( 'footer_col_2' ); ?>
+        <?php ob_start(); dynamic_sidebar('footer_col_1'); $footer_col_1 = ob_get_clean(); 
+          echo $footer_col_1 ? $footer_col_1 :'';
+        ?>
+        <?php ob_start(); dynamic_sidebar('footer_col_2'); $footer_col_2 = ob_get_clean(); 
+          echo $footer_col_2 ? $footer_col_2 :'';
+        ?>
+        <?php ob_start(); dynamic_sidebar('footer_col_3'); $footer_col_3 = ob_get_clean(); 
+          echo $footer_col_3 ? $footer_col_3 :'';
+        ?>
+        <?php ob_start(); dynamic_sidebar('footer_col_4'); $footer_col_4 = ob_get_clean(); 
+          echo $footer_col_4 ? $footer_col_4 :'';
+        ?>
       </div>
     </div>
   </footer>
